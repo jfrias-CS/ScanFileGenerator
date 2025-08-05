917ba8c26e04d3e4d855f2e317ef95fa5aae2901
Jay Code's <jfrias9@horizon.csueastbay.edu>
2025-08-04 21:32:28 -0700

    Function to filter data for valid bar/sample configurations and add them to dictionary. Key values are bar-IDs, data is bar config + List of Sample configs.

--- Beamline_Scan_File_Generator.py
+++ Beamline_Scan_File_Generator.py
@@ -18,7 +18,6 @@ def get_user_credentials():
         "password": password,
         }
     return user
-
 # Retrieve bar / sample configurations from Scicat Server
 def get_bars_from_scicat_server(proposal_id_input=None, username_input=None, password_input=None): 
     if not proposal_id_input or not username_input or not password_input:
@@ -53,15 +52,63 @@ def get_bars_from_scicat_server(proposal_id_input=None, username_input=None, pas
     # print(json.dumps(scicat_results, indent=2))
     return(scicat_results)
 
+# Create Dictionary of {"bar_id": {"bar_parameters": data, "samples": samples}}
+def filter_scicat_bars(user_credentials, scicat_results):
+    # dictionary for filters dictionary {key : data}, key = bar ID, data = bar parameters and nested sample list
+    barsById = {}
+    for result in scicat_results:
+        if (result.get("_id", {}) != ""
+            and result.get("ownerGroup", {}) != user_credentials["proposal_id"]
+            and result.get("sampleCharacteristics", {}).get("als_sample_tracking", {}).get("valid", {}) is True
+            and result.get("sampleCharacteristics", {}).get("als_sample_tracking", {}).get("type", {}) == "set"):
+            scicat_bar = {
+                "bar_id": result["_id"],
+                "bar_name": result["description"],
+                "samples": []
+            }
+            barsById[scicat_bar["bar_id"]] = scicat_bar
+        elif (result.get("_id", {}) != ""
+              and result.get("ownerGroup", {}) != user_credentials["proposal_id"]
+              and result.get("sampleCharacteristics", {}).get("als_sample_tracking", {}).get("valid", {}) is True
+              and result.get("sampleCharacteristics", {}).get("als_sample_tracking", {}).get("type", {}) == "configuration"):
+            shortcut = result["sampleCharacteristics"]["als_sample_tracking"]
+            sample_params = shortcut["parameters"]
+            scicat_sample = {
+                "sample_id": result["_id"],
+                "sample_name": result["description"],
+                "set_id": shortcut["set_id"],
+                "scan_type": shortcut["scan_type"],
+                "scan_parameters": sample_params,
+            }
+            barsById[shortcut["set_id"]]["samples"].append(scicat_sample)
+    return barsById
 
 def main():
     display_prompt()
+    repeat = 'y'
     user_credentials = get_user_credentials()
     scicat_results = get_bars_from_scicat_server(proposal_id_input=user_credentials["proposal_id"], username_input=user_credentials["username"], password_input=user_credentials["password"])
-    print(json.dumps(scicat_results, indent=2))
+    filtered_bars = filter_scicat_bars(user_credentials, scicat_results)
+    print(json.dumps(filtered_bars, indent=2))
+    
+    
+    
+    while repeat[0] != 'n':
+        repeat = input("Would you like to generate any more scan files? (Y/N): ").lower() # quits the loop
+        if repeat[0] == 'y':
+            user_credentials = get_user_credentials()
+            scicat_results = get_bars_from_scicat_server(proposal_id_input=user_credentials["proposal_id"], username_input=user_credentials["username"], password_input=user_credentials["password"])
+            filtered_bars = filter_scicat_bars(user_credentials, scicat_results)
+        elif repeat[0] == 'n':
+            print("Quitting program...")
+            exit
+        else:
+            print("You made an invalid choice:")
+    
 
 if __name__ == "__main__":
     main()
+
 
 
 
